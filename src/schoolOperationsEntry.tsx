71a2c963203c72e0411cc76313cd1d5710f025fe
Sobhy Mahmoud Ahmed <mr.memo87@gmail.com>
2026-08-10 15:56:10 +0300

    Fix School Operations stylesheet import

--- src/schoolOperationsEntry.tsx
+++ src/schoolOperationsEntry.tsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '../index.css';
+import './index.css';
 import SchoolOperationsPage from '../components/school-operations/SchoolOperationsPage';
 
 const root = document.getElementById('root');
